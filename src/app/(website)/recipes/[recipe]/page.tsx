adff6335ec80d701832cd71ff1abf4c48bdb643e
milosdjurica <milosdjurica13@gmail.com>
2023-11-02 23:51:36 +0100

    stylings to the single recipe page

--- src/app/(website)/recipes/[recipe]/page.tsx
+++ src/app/(website)/recipes/[recipe]/page.tsx
@@ -11,36 +11,61 @@ type Props = {
 export default async function RecipePage({ params }: Props) {
   const id = params.recipe;
   const recipe = await getSingleRecipe(id);
-  console.log(recipe.primaryImage);
+
   return (
     <div className="space-y-6 p-6">
       <h1 className="text-center text-5xl font-semibold text-primary">
         {recipe.title}
       </h1>
-      <div className="relative mx-auto flex h-[400px] w-full overflow-hidden md:w-3/4 xl:w-1/2">
+      <div className="relative flex h-[500px] w-full">
         <Image
           src={recipe.primaryImage}
           alt={recipe.title}
           fill
           priority
-          className="rounded-lg  object-cover"
+          className="rounded-lg border border-primary object-cover"
         />
       </div>
+      <div className="flex flex-col items-center  justify-around md:flex-row">
+        <div>
+          {/* // TODO put range GREEN YELLOW RED -> easy/medium/hard */}
+          <h2>Difficulty: {recipe.difficulty}</h2>
+        </div>
+        <h2>
+          Expected cost: {recipe.price?.count} {recipe.price?.unit}
+        </h2>
+        <div>
+          {/* // TODO add  */}
+          <h2>
+            Average time: {recipe.time?.count} {recipe.time?.unit}
+          </h2>
+        </div>
+      </div>
 
-      <PortableText value={recipe.description} />
-      <h3>Ingredients: </h3>
-      <ol>
+      <div className="space-y-4 text-lg">
+        <h2 className="text-2xl underline decoration-primary">Description</h2>
+        <PortableText value={recipe.description} />
+      </div>
+      <h2 className="text-2xl underline decoration-primary">Ingredients</h2>
+      <ol className="list-inside list-disc">
         {recipe.ingredients.map((ingredient) => (
           <li key={ingredient.recipe_ingredient_id}>{ingredient.name}</li>
         ))}
       </ol>
-      <h4>Difficulty: {recipe.difficulty}</h4>
-      <h4>
-        Expected cost: {recipe.price?.count} {recipe.price?.unit}
-      </h4>
-      <h4>
-        Average time: {recipe.time?.count} {recipe.time?.unit}
-      </h4>
+
+      {recipe.secondaryImage && (
+        <Image
+          src={recipe.secondaryImage.url}
+          alt={recipe.secondaryImage.alt}
+          width={300}
+          height={300}
+        />
+      )}
+
+      <div className="space-y-4 text-lg">
+        <h2 className="text-2xl underline decoration-primary">Instructions</h2>
+        <PortableText value={recipe.instructions} />
+      </div>
     </div>
   );
 }
