05676b955b7880a114f00dc631452651bdbe92fa
milosdjurica <milosdjurica13@gmail.com>
2023-11-02 19:49:38 +0100

    added ingredients dialog, and changed AllRecipes from grid to flex, also now user can only click button if want to see recipe

--- src/components/AllRecipesComponent.tsx
+++ src/components/AllRecipesComponent.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { PortableText } from "@portabletext/react";
 
 import { getRecipes } from "@/sanity/sanity.utils";
 import { Button } from "./ui/button";
+import { IngredientsDialog } from "./IngredientsDialog";
 
 export default async function AllRecipesComponent() {
   const recipes = await getRecipes();
 
   return (
-    // TODO  maybe change to flex
-    <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    // TODO  maybe change to grid
+    <div className="flex flex-wrap items-center justify-center gap-10 p-10">
       {recipes.map((recipe) => (
-        <Link
+        <div
           key={recipe._id}
-          // ! maybe will have conflict if 2 recipes have same name
-          href={`/recipes/${recipe.slug}`}
           className="w-[350px] space-y-2 rounded-lg bg-secondary p-5
           shadow-md shadow-primary transition ease-in hover:scale-105"
         >
           <h3 className="text-2xl font-semibold">{recipe.title}</h3>
-          {/* // TODO on See ingredients open popup or something to show all ingredients */}
-          {/* // TODO scroll area/popover/hover card/dialog/sheet with description and click to see recipe?   */}
-          <Button variant="link" className="p-0">
-            See ingredients
-          </Button>
+          <IngredientsDialog
+            slug={recipe._id}
+            description={recipe.description}
+            ingredients={recipe.ingredients}
+          />
+
           <Image
             src={recipe.primaryImage}
             alt={recipe.title}
@@ -45,24 +44,12 @@ export default async function AllRecipesComponent() {
               {recipe.price?.count} {recipe.price?.unit}
             </h6>
           </div>
-        </Link>
+          <Button variant="default">
+            {/* // ! maybe recipe.slug but it will have conflict if 2 recipes have same name */}
+            <Link href={`recipes/${recipe.slug}`}>See recipe</Link>
+          </Button>
+        </div>
       ))}
     </div>
   );
 }
-
-// <div className="h-[400px] w-[400px] space-y-4 rounded-lg bg-secondary p-2">
-// <h3 className="text-xl font-semibold">{recipe.title}</h3>
-// <p className=""> {recipe.difficulty}</p>
-// <Button variant="link" className="p-0">
-//   See ingredients
-// </Button>
-// <div className="">
-//   <Image
-//     src={recipe.primaryImage}
-//     alt={recipe.title}
-//     width={200}
-//     height={150}
-//     className="max-h-[200px] w-full border border-primary object-cover"
-//   />
-// </div>
